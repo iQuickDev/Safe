fe71b547ee576feb3092c8ae196defedc3c86e73
iQuick <iquickpublic@gmail.com>
2022-12-21 18:20:52 +0100

    help message + add function

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const aes = require('aes256')
 const tar = require('tar')
 const path = require('path')
 const fs = require('fs')
+const colors = require('colors')
 const prompt = require('prompt')
-prompt.message = '[INPUT] '
+prompt.message = '[INPUT] '.green
+prompt.delimiter = ''
 
 /* FLAGS
 *   -a <archive> <file1> <file2> <fileN>: adds a file to the archive
@@ -16,15 +18,54 @@ prompt.message = '[INPUT] '
 
 async function setPassword() {
     prompt.start()
-    settings.password = (await prompt.get({ name: 'Password', required: true, hidden: true })).Password
+    try {
+        settings.password = (await prompt.get({
+            name: 'password',
+            required: true,
+            hidden: true,
+            message: "Password:"
+        })).password
+    } catch (e) {
+        console.error("\n[ERROR] The program was forcefully closed".red)
+        process.exit(1)
+    }
+}
+
+function displayHelp() {
+    console.log('Usage: safe [OPTION]... [FILE]...\n'.cyan)
+    console.log('Options:'.yellow)
+    console.log('  -a <archive> <file1> <file2> <fileN>: adds a file to the archive'.green);
+    console.log('  -e <archive> <destination>: extracts the archive'.green);
+    console.log('  -r <archive> <file1>: removes a file from the archive'.green);
+    console.log('  -c <name> <file1> <file2> <fileN>: creates an archive'.green);
+    console.log('  -v <archive>: view the contents of the archive'.green);
+    console.log('  -p <password>: provide the archive password\n'.green);
+}
+
+function checkArchive(archive)
+{
+    if (!fs.existsSync(archive)) {
+        if (!fs.existsSync(`${archive}.tar.safe`)) {
+            console.error('[ERROR] Archive does not exist'.red)
+            process.exit(1)
+        }
+        archive += '.tar.safe'
+    }
+
+    if (!archive.endsWith('.tar.safe')) {
+        console.error("[ERROR] Archive is not encrypted or extension has been removed".red)
+        process.exit(1)
+    }
+
+    return
 }
 
 const functions = {
     c: function create(name, files) {
         tar.create(
             {
-                gzip: true,
-                file: `${name}.tar.gz`
+                gzip: false,
+                file: `${name}.tar`
             },
             files
         )
@@ -32,16 +73,44 @@ const functions = {
                 if (!settings.password)
                     await setPassword()
 
-                fs.writeFileSync(`${name}.tar.gz.safe`, aes.encrypt(settings.password, fs.readFileSync(`${name}.tar.gz`)))
+                fs.writeFileSync(`${name}.tar.safe`, aes.encrypt(settings.password, fs.readFileSync(`${name}.tar`)))
+                fs.unlinkSync(`${name}.tar`)
+                console.log(`[SUCCESS] The archive ${name.bold} was successfully created`.green)
             })
     },
 
-    a: function add(archive, file) {
-        console.log('called the add function')
+    a: async function add(archive, files) {
+
+        checkArchive(archive)
+
+        if (!settings.password)
+            await setPassword()
+
+        let decryptedArchive = aes.decrypt(settings.password, fs.readFileSync(archive))
+        fs.writeFileSync(`${archive.replaceAll('.safe', '')}`, decryptedArchive)
+        
+        for (const file of [...files])
+        {   
+            if (!fs.existsSync(file))
+            {
+                files.splice(files.indexOf(file), 1)
+                console.error(`[WARNING] File ${file} will be ignored because it does not exist`.yellow)
+            }
+        }
+
+        await tar.update({
+            gzip: false,
+            file: `${archive.replaceAll('.safe', '')}`
+        }, files)
+        .then(async () => {
+            console.log(`[SUCCESS] Added ${files.toString()} to the archive ${archive.bold}`.green)
+            fs.writeFileSync(`${archive}`, aes.encrypt(settings.password, fs.readFileSync(archive.replaceAll('.safe', ''))))
+            fs.unlinkSync(archive.replaceAll('.safe', ''))
+        })
     },
 
     e: function extract(archive) {
-        console.log('called the extract function')
+        checkArchive(archive)
     },
 
     r: function remove() {
@@ -51,10 +120,6 @@ const functions = {
     v: function view() {
         console.log('called the view function')
     },
-
-    d: function decrypt(password) {
-        console.log('called the decrypt function')
-    }
 }
 
 const possibleFlags = [
@@ -63,7 +128,8 @@ const possibleFlags = [
     { name: 'r', length: Infinity },
     { name: 'c', length: Infinity },
     { name: 'v', length: 1 },
-    { name: 'p', length: 1 }
+    { name: 'p', length: 1 },
+    { name: 'verbose', length: 0 }
 ]
 
 let args = []
@@ -71,6 +137,10 @@ let args = []
 for (let i = 0; i < process.argv.length; i++) {
     if (process.argv[i].startsWith('-')) {
         const flag = process.argv[i].replace('-', '')
+        if (flag == 'h') {
+            displayHelp()
+            process.exit(0)
+        }
         if (possibleFlags.find(f => f.name == flag)) {
             let params = []
             for (let j = 1; j <= possibleFlags.find(f => f.name == flag).length; j++) {
@@ -93,14 +163,30 @@ if (args.length == 0) {
     process.exit(1)
 }
 
+if (args.length > 2) {
+    console.log("too many arguments... type safe -h to display a help message")
+    process.exit(1)
+}
+
 const settings = {
-    password: args.find(a => a.flag == 'p').params[0],
-    action: args.find(a => a.flag != 'p').flag
+    action: args.find(a => a.flag != 'p').flag,
+    verbose: false
+}
+
+if (args.find(a => a.flag == 'p')) {
+    Object.assign(settings, {
+        password: args.find(a => a.flag == 'p').params[0]
+    })
+}
+
+if (args.find(a => a.flag == 'verbose')) {
+    Object.assign(settings, {
+        verbose: true
+    })
 }
 
 let createdParams = args.find(arg => arg.flag == settings.action).params
 functions[settings.action](createdParams.shift(), createdParams)
 
-//todo: parser recognizes too many args
 //todo: configure prompt
 //todo: better error messages
